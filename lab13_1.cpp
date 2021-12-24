db9779c1b3c69afcfcddbb2f5a3142f8be2d02c3
siksaka09 <earthsik09@gmail.com>
2021-12-24 18:01:46 +0700

    lab13 all completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,34 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
+long long int fibonacci(long long int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+
+
+long long int fibonacci(long long int z){
+    if(z == 0){
+        return 0;
+
+    }
+    else if(z == 1)
+    {
+        return 1;
+    }
+    else if(z >1)
+    {
+        return fibonacci(z-1) + fibonacci(z-2);
+    }
+    else{
+        return 0;
+    }
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+
+int gcd(int x , int y){
+    if(x%y == 0){
+        return y;
+    }
+    
+    else
+    {
+        int z = y;
+        y = x%z;
+        return gcd(z,y);
+
+    }
+
 }
